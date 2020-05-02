8c7f23ad04dfa2b29009f1597cf860ef70ba3eb8
Oswald Calvario <oswald.calvario@gmail.com>
2020-05-02 03:13:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,5 @@
 # code your #position_taken? method here
 
 def position_taken? (board, index)
-  taken = nil
-  
-  if (board[index] == " " || board[index] == "" || board[index] == nil)
-    taken = false
-  else
-    taken = true
-end
+
 end
